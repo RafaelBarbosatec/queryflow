0be8af0f054e2d74e454c9ab4548bcbc5d6c95c2
rafaelalmeidacloudwalk <rafael.almeida@cloudwalk.io>
2025-03-31 15:09:06 -0300

    feat: add MatchResult class to encapsulate query and parameters

--- lib/src/builders/matcher.dart
+++ lib/src/builders/matcher.dart
@@ -1,3 +1,9 @@
+class MatchResult {
+  final String query;
+  final List<String> params;
+  MatchResult(this.query, this.params);
+}
+
 abstract class BaseMatcher {
   String compose(String current);
 }
